e7d5d1dfea8a39be34c7928b31dfedce67e1a7d0
Maike Menezes <maikermenezes@gmail.com>
2023-05-24 16:12:58 -0300

    carrossel

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { HomeContainer } from './styles';
+import React from 'react';
+import { HomeContainer, HomeButton, NewHomeButton } from './styles';
 import { LogoCITi } from '../../assets';
+import { Carousel } from '../../components/Carousel';
 
-
-export const Home: React.FC = () => {
-    return (
-        <HomeContainer>
-            <img src={LogoCITi} alt="" />
-            <h1>ReactJS Boilerplate</h1>
-            <p>
-                Made with
-                {' '}
-                <strong>&lt; &#x0002F; &gt;</strong>
-                {' '}
-                and
-                {' '}
-                <strong>&hearts;</strong>
-                {' '}
-                by CITi
-            </p>
-        </HomeContainer>
-    );
-}
+export const Home = () => {
+  return (
+    <HomeContainer>
+      {/* <HomeImg
+        src={LogoCITi}
+        alt=''
+      />
+      <HomeH1>ReactJS Boilerplate</HomeH1>
+      <HomeP>Made with &lt; &#x0002F; &gt; and &hearts; by CITi</HomeP> */}
+      <Carousel />
+      <NewHomeButton>Botão vermelho</NewHomeButton>
+      <HomeButton>Botão 1</HomeButton>
+      <HomeButton>Botão 2</HomeButton>
+      <HomeButton>Botão 3</HomeButton>
+    </HomeContainer>
+  );
+};
--- client/src/pages/home/styles.ts
+++ client/src/pages/home/styles.ts
@@ -1,5 +1,4 @@
-import styled from 'styled-components'; 
-
+import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
   width: 100vw;
@@ -8,32 +7,49 @@ export const HomeContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 20px;
   flex-direction: column;
+`;
 
-  h1 {
-    font-size: 54px;
-    color: ${(props) => props.theme.colors.primary};
-    margin-top: 40px;
-    text-align: center;
+export const HomeH1 = styled.h1`
+  font-size: 54px;
+  color: ${(props) => props.theme.colors.primary};
+  margin-top: 40px;
+  text-align: center;
 
-    @media (max-width: 720px) {
-      font-size: 28px;
-    }
+  @media (max-width: 720px) {
+    font-size: 28px;
   }
-  
-  p {
+`;
 
-    font-size: 20px;
-    margin-top: 24px;
-    line-height: 32px;
+export const HomeP = styled.p`
+  font-size: 20px;
+  margin-top: 24px;
+  line-height: 32px;
 
-    @media (max-width: 720px) {
-      font-size: 16px;
-      margin-top: 0;
-    }
+  @media (max-width: 720px) {
+    font-size: 16px;
+    margin-top: 0;
   }
+`;
 
-  img {
-    width: 25%;
-  }
+export const HomeImg = styled.img`
+  width: 25%;
+`;
+
+export const HomeButton = styled.button`
+  width: 200px;
+  height: 60px;
+  border: none;
+  color: white;
+  border-radius: 8px;
+  border-style: solid;
+  border-color: yellow;
+  border-width: 5px;
+  font-size: 24px;
+  background-color: blue;
+`;
+
+export const NewHomeButton = styled(HomeButton)`
+  background-color: red;
 `;
